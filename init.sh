07333b66b9b34a691b7cada3a008f50f0961df8d
Matheus Henrique Raymundo <matheus.raymundo2@br.experian.com>
2020-12-06 16:05:12 -0300

    Adjust init script

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 docker network create --driver=bridge heroes_network
 cd database
 docker build -t heroes-mysql .
-docker run -d -p 3306:3306 --name heroes-mysql -e MYSQL_ROOT_PASSWORD=superheroes --network heroes_network heroes-mysql
+docker run -d -p 3306:3306 --name heroes-mysql --network heroes_network heroes-mysql
